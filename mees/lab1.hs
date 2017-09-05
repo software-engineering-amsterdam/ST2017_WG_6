dd93cd1aeb4df160fc5ad2ab839ee131b57879cd
Meess <abcmaster8@hotmail.com>
2017-09-05 10:20:53 +0200

    Lab 1 finished exercise 2 and 3

--- mees/lab1.hs
+++ mees/lab1.hs
@@ -5,7 +5,8 @@ import Test.QuickCheck
 
 
 -- ##########
--- # Exercise 1 
+-- # Lab 1 Exercise 1 
+-- # 5 hour(s)
 -- ##########
 
 fac2 :: Int -> Int
@@ -23,7 +24,7 @@ ex_1 n = fac2 n == proof2 n
 (-->) :: Bool -> Bool -> Bool
 p --> q = (not p) || q
 
-test1 = quickCheckResult (\n -> (n >= 0) --> (fac2 n == proof2 n))
+test11 = quickCheckResult (\n -> (n >= 0) --> (fac2 n == proof2 n))
 
 fac3 :: Int -> Int
 fac3 0 = 0
@@ -33,35 +34,30 @@ proof3 :: Int -> Int
 proof3 n = (n * (n + 1) `div` (2)) ^ 2
 
 
-test2 = quickCheckResult (\n -> (n >= 0) --> (fac3 n == proof3 n))
--- ex_2 = 
+test12 = quickCheckResult (\n -> (n >= 0) --> (fac3 n == proof3 n))
 
+-- ##########
+-- # Lab 1 Exercise 2
+-- # 1,5 hour(s)
+-- ##########
 
--- # https://mail.haskell.org/pipermail/beginners/2011-May/007122.html
--- \
--- fac3 0 = 0
--- fac3 n = n ^ 3 + fac3 (n - 1)
+sub_eq_n2 :: Int -> Bool
+sub_eq_n2 n = length(subsequences[1..n]) == 2 ^ n
 
--- proof3 n = (quot (n * (n + 1) (2))) ^ 2
+test2 = quickCheckResult (\n -> (n >= 0) --> (sub_eq_n2 n))
 
+-- ##########
+-- # Lab 1 Exercise 3
+-- # 0.5 hour(s)
+-- ##########
 
-    
--- p xs -> all p xs == myall p xs
+-- num_perms_eq :: Int -> Bool
+-- num_perms_eq n = length(permutations[1..n]) == product[1..n]
 
--- prime :: Integer -> Bool
--- prime n = n > 1 && all (\ x -> rem n x /= 0) xs
---   where xs = takeWhile (\ y -> y^2 <= n) primes
--- primes :: [Integer]
--- primes = 2 : filter prime [3..] 
--- a = "abs"
+test3 = quickCheckResult (\n -> (n >= 0) -->
+                         (length(permutations[1..n]) == product[1..n]))
 
--- myall :: (a -> Bool) -> [a] -> Bool
--- myall p [] = True
--- myall p (x:xs) = p x && myall p xs
-
--- list2p :: Eq a => [a] -> a -> Bool
--- list2p = flip elem
-
--- myallTest :: [Int] -> [Int] -> Bool
--- myallTest = \ ys xs -> let p = list2p ys in
---  all p xs == myall p xs
+-- ##########
+-- # Lab 1 Exercise
+-- # hour(s)
+-- ##########
