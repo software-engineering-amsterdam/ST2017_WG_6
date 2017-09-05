9a6615c103ce2312f1fc6f6224814fbae8dd32a5
NL\SangaGupta <sangagupta@deloitte.nl>
2017-09-05 17:40:42 +0200

    Added excercise 1 - 7

---
+++ combined deliverables/lab1.hs
@@ -0,0 +1,251 @@
+module Lab1 where
+import Data.List
+import Test.QuickCheck
+
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+forall :: [a] -> (a -> Bool) -> Bool
+forall = flip all
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+1.) Redo exercises 2 and 3 of Workshop 1 by writing QuickCheck tests for these statements.
+    See the end of Lecture 1 for how this can be done.
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+exercise2a :: Int -> Int
+exercise2a n = sum(squares n)
+
+squares :: Int -> [Int]
+squares n = map (^2) [1..n]
+
+exercise2b :: Int -> Int
+exercise2b n = (n * (n + 1) * (2 * n + 1)) `div` 6
+
+-- QuickCheck test
+testExercise2 n = (\n -> n >= 0 --> exercise2a n == exercise2b n)
+
+
+exercise3a :: Int -> Int
+exercise3a n = sum(cubes n)
+
+cubes :: Int -> [Int]
+cubes n = map (^3) [1..n]
+
+exercise3b :: Int -> Int
+exercise3b n = (n * (n + 1) `div` 2) ^2
+
+-- QuickCheck test
+testExercise3 n = (\n -> n >= 0 --> exercise3a n == exercise3b n)
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+2.) Redo exercise 4 of Workshop 1 by replacing sets by lists, and testing the property for integer lists of the form [1..n].
+    You can use subsequences :: [a] -> [[a]] for the list of all subsequences of a given list.
+
+    'Prove by induction that if A is a finite set with |A|=n, then |P(A)|=2n.'
+--------------------------------------------------------------------------------------------------------------------------------------}
+sub_eq_n2 :: Int -> Bool
+sub_eq_n2 n = length(subsequences[1..n]) == 2 ^ n
+
+test2 = quickCheckResult (\n -> (n >= 0 && n < 20) --> (sub_eq_n2 n))
+{-------------------------------------------------------------------------------------------------------------------------------------
+----'Is the property hard to test? If you find that it is, can you given a reason why?'
+
+    It's pretty simple, but it turned out that it'll take 'forever' if you don't limit QuickCheck's input value.
+    At a max of 20 it runs fine, at >30 it's significantly slow.
+    The funcion itself grows exponential with n.
+
+
+----'Give your thoughts on the following issue: when you perform the test for exercise 4, what are you testing actually?
+----Are you checking a mathematical fact? Or are you testing whether subsequences satisfies a part of its specification?
+----Or are you testing something else still?'
+
+    You are indeed testing both a mathematical fact and the specification of subsequence by using that fact.
+    You could say that you're also testing calculation speed when you don't limit QuickCheck's input values,
+    but then you would actually have to measure the time it takes and compare that to some expectation for it to make sense.
+
+ --------------------------------------------------------------------------------------------------------------------------------------}
+
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+3.) Redo exercise 5 of Workshop 1 by replacing sets by lists, and testing the property for integer lists of the form [1..n].
+----A permutation of a list is a reordering of the members of a list.
+----You can also use the Data.List function permutations.
+----Find a formula (closed form) for the number of permutations of a list of n distinct objects, and prove your guess by induction.
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+test3 = quickCheckResult (\n -> (n >= 0) -->
+                         (length(permutations[1..n]) == product[1..n]))
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+----'Is the property hard to test? If you find that it is, can you given a reason why?'
+    Yes, because generating permtations is O
+
+----Again, give your thoughts on the following issue: when you perform the test for exercise 5, what are you testing actually?
+----Are you checking a mathematical fact? Or are you testing whether perms satisfies a part of its specification?
+----Or are you testing something else still?
+
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+4.) The natural number 13 has the property that it is prime and its reversal, the number 31, is also prime.
+    Write a function that finds all primes < 10000 with this property.
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+reversal :: Integer -> Integer
+reversal = read . reverse . show
+{--
+By defenition '-->' is only returns False if the first condition is True and
+the second function is False. Since reversal reverses wrong on negative Integers
+and Intergers ending on a zero, 1000 reverses in 1. But since all Integers
+ending with a zero are even numbers they can't be primes and primes can't be
+negative these are automatically returning True. That's why the test
+always passes which approves this statement.
+--}
+--check_reversal :: Integer -> Bool
+check_reversal = \n -> prime n --> (reversal(reversal n) == n)
+
+test4_reversal = quickCheck (check_reversal)
+
+
+prime :: Integer -> Bool
+prime n = n > 1 && all (\ x -> rem n x /= 0) xs
+  where xs = takeWhile (\ y -> y^2 <= n) primes
+
+primes :: [Integer]
+primes = 2 : filter prime [3..]
+
+primes_eq_reversal :: Integer -> [Integer]
+primes_eq_reversal n = takeWhile ( < n ) (filter (prime . reversal) primes)
+
+-- prime_eq_reverse :: Integer -> Bool
+-- prime_eq_reverse n = (prime n) == (prime (reverseInt n))
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+    'How would you test this function, by the way?'
+    I would want to check whether every number with at least two digits that reversiblePrimes returns has a buddy.
+    This could be done by checking for reversals in the list, or possibly even faster when iterating through the
+    list x the list, though Haskell's lists appear to be singly-linked and queue-like (because you can easily take
+    heads) so it takes a full iteration to reach the last element.
+    This isn't suitable for a QuickCheck test because this isn't about testing against random values, we're using
+    the same subset of the set of all primes < 10000 each time.
+
+    I could probably attempt a bit like a unittest?
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+Performance could probably be better, but testing this with reversiblePrimes returns True,
+testing it with (reversiblePrimes ++ [12]) returns False.
+
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+5.) The number 101 is a prime, and it is also the sum of five consecutive primes, namely 13+17+19+23+29.
+    Find the smallest prime number that is a sum of 101 consecutive primes.
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+sublistPrimes :: [Integer] -> Bool
+sublistPrimes n = prime (sum n)
+
+recursiveReturnSublist :: Int -> [a] -> [[a]]
+recursiveReturnSublist n l = (take n l) : recursiveReturnSublist n (tail l)
+
+answer5 = sum (head (filter sublistPrimes (recursiveReturnSublist 101 primes)))
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+----'Do you have to test that your answer is correct? How could this be checked?'
+     That would be wise though I'm not sure how.
+     I compared my output with that of my teammates. Our approaches are different but the resulting prime number is the same.
+
+--------------------------------------------------------------------------------------------------------------------------------------}
+
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+6.) Using Haskell to refute a conjecture.
+    Write a Haskell function that can be used to refute the following conjecture.
+    "If p1,...,pn is a list of consecutive primes starting from 2, then (p1×⋯×pn)+1 is also prime."
+    This can be refuted by means of a counterexample, so your Haskell program should generate counterexamples.
+--------------------------------------------------------------------------------------------------------------------------------------}
+recursiveReturnSublist2 :: Int -> [a] -> [[a]]
+recursiveReturnSublist2 n l = (take n l) : recursiveReturnSublist2 (n+1) l
+
+conjecture :: [Integer] -> Bool
+conjecture xs = prime ((product xs) + 1)
+
+answer6 = head (filter (not.conjecture) (recursiveReturnSublist2 1 primes))
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+    What is the smallest counterexample?
+    > take 1 counterExamples
+    [7]
+--------------------------------------------------------------------------------------------------------------------------------------}
+luhn :: Integer -> Bool
+luhn n = (sum (replaceDigits (intToList n)) `mod` 10) == 0
+
+replaceDigits :: [Integer] -> [Integer]
+replaceDigits [] = []
+replaceDigits [x] = [x]
+replaceDigits (x:y:zs) = x : modDigit y : replaceDigits zs
+
+modDigit :: Integer -> Integer
+modDigit y
+    | y < 5     = 2*y
+    | otherwise = (2*y) - 9
+
+intToList :: Integer -> [Integer]
+intToList n
+    | n < 10    = [n]
+    | otherwise = [n `mod` 10] ++ intToList (n `div` 10)
+
+
+isAmericanExpress, isMaster, isVisa :: Integer -> Bool
+isMaster n = length(intToList n) == 16 &&
+             n1 == 5 &&
+             elem n2 [1..5] &&
+             luhn n
+             where (n1:n2:ns) = intToList n
+
+isVisa n = n1 == 4 &&
+           (len == 13 || len == 16 || len == 19) &&
+           luhn n
+           where (n1:ns) = intToList n; len = length(intToList n)
+
+isAmericanExpress n = length(intToList n) == 15 &&
+                      n1 == 3 &&
+                      (n2 == 4 || n2 == 7) &&
+                      luhn n
+                      where (n1:n2:ns) = intToList n
+
+-- SOURCE : http://www.getcreditcardnumbers.com/
+visaValid = [4532530353861276,
+             4539091191338186,
+             4532660285963153,
+             4485675506393284,
+             4716050923148342]
+
+masterValid = [5561182643231042,
+               5344239834935031,
+               5487320019367914,
+               5475210985327105,
+               5357091271641828]
+
+americaValid = [346676877003012,
+                348288126992371,
+                379346372653337,
+                372551003187871,
+                377701033813435]
+
+visa = map isVisa visaValid
+master = map isMaster masterValid
+america = map isAmericanExpress americaValid
+
+{-------------------------------------------------------------------------------------------------------------------------------------
+7.) Implement and test the Luhn Algorithm
+    The Luhn algorithm is a formula for validating credit card numbers.
+    Give an implementation in Haskell. The type declaration is given.
+    This function should check whether an input number satisfies the Luhn formula.
+--------------------------------------------------------------------------------------------------------------------------------------}
+--    luhn :: Integer -> Bool
