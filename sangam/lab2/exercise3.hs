517c20fa1982e3dd11abbf73edd8d312206aae07
NL\SangaGupta <sangagupta@deloitte.nl>
2017-09-12 10:56:40 +0200

    Cleanup

--- sangam/lab2/exercise3.hs
+++ sangam/lab2/exercise3.hs
@@ -15,17 +15,16 @@ a)  Implement all properties from the Exercise 3 from Workshop 2 as
 
 --------------------------------------------------------------------------------------------------------------------------------------}
 
-property1, property2, property3, property4 :: Int -> Bool
+property1, property2, property3 :: Int -> Bool
 property1 = (\ x -> even x && x > 3)
 property2 = (\ x -> even x || x > 3)
 property3 = (\ x -> (even x && x > 3) || even 3)
-property4 = (\ x -> (even x && x > 3) || even 3) --Property 4 is the same as 3 (\ x -> (even x && x > 3) || even x)=
 
 -- Which of the following properties is stronger? assume domain [1..10]
 compare1 = compar [1..10] property1 even -- Result property1 is stronger than even
 compare2 = compar [1..10] property2 even -- Result property2 is weaker than even
 compare3 = compar [1..10] property3 even -- Result property3 is stronger than even
-compare4 = compar [1..10] even property4 -- Result even is weaker than property4
+compare4 = compar [1..10] even property3 -- Result even is weaker than property3
 
 
 {-------------------------------------------------------------------------------------------------------------------------------------
@@ -34,7 +33,7 @@ b)  Provide a descending strength list of all the implemented properties.
 -- For convience I made a list of tuples containing all the properties with an int equivalent to the property
 -- I did this so I can print the int in a list
 allProperties :: [(Int, Int -> Bool)]
-allProperties = [(1, property1), (2, property2), (3, property3), (4, property4), (5, even)]
+allProperties = [(1, property1), (2, property2), (3, property3), (4, even)]
 
 -- Modified stronger to work with quickSortProperties.
 strongerNotEquivalent :: (Int->Bool) -> (Int -> Bool) -> Bool
@@ -51,13 +50,13 @@ quickSortProperties (x:xs) =
    ++ quickSortProperties [ a | a <- xs, weaker [(-10)..10] (snd a) (snd x) ]
 
 exercise3 = quickSortProperties allProperties
-{-- the result of exercise3 is [1, 3, 4, 5, 2]
+{-- the result of exercise3 is [1, 3, 4, 2]
     The int in the list correspond to the properties. To check if the list produced by exercise3 is truly correct,
-    I checked if result is correct with compar. Which gave me 1 eq 3, 3 eq 4 (because they are same), 4 stronger than 5 and 5 stronger than 2.
+    I checked if result is correct with compar. Which gave me 1 eq 3, 3 stronger than 4 and 4 stronger than 2.
 
     This also means that:
-     1 is eq to 4 and stronger than 5 and 2
-     3 is eq to 1 and stronger than 5 and 2
+     1 is stronger than 4 and 2
+     3 is eq to 1 and stronger than 2
      etc.
 
     Which is also true according to compar.
