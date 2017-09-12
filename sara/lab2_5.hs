79f043bb2445fdcf10944e5bcc1ecd0cfd0511bd
sara <sara@mollie.com>
2017-09-12 14:37:09 +0200

    Finished 5

--- sara/lab2_5.hs
+++ sara/lab2_5.hs
@@ -6,11 +6,6 @@ import System.Random
 import Test.QuickCheck
 import Lecture2
 
-infix 1 -->
-
-(-->) :: Bool -> Bool -> Bool
-p --> q = (not p) || q
-
 
 {-
     Implementing and testing ROT13 encoding
@@ -18,10 +13,50 @@ p --> q = (not p) || q
     ROT13 is a single letter substitution cipher that is used in online forums for hiding spoilers. See also www.rot13.com.
 
     First, give a specification of ROT13.
+----------
+"Each English alphabetic letter is replaced by the letter 13 places further along in the alphabet,
+cycling back to the start when passing the 26th character 'z'. The fact that there are 26 English alphabetic characters and
+that 26 / 2 = 13, ROT13 inverses the letters, meaning applying it twice will result in the same string as before."
+----------
 
     Next, give a simple implementation of ROT13.
 
     Finally, turn the specification into a series of QuickCheck testable properties, and use these to test your implementation.
 
-    10:30
+    2h 30m
+
+    Source used: https://hackage.haskell.org/package/base-4.7.0.0/docs/Data-Char.html
+
 -}
+
+rot13 :: [Char] -> [Char]
+rot13 [] = []
+rot13 (c:cs) = (rot c) : (rot13 cs)
+
+rot :: Char -> Char
+rot c | (isLetter c) = toEnum (newCharIndex c) :: Char
+      | otherwise = c
+
+
+newCharIndex :: Char -> Int
+newCharIndex c | (isAsciiLower c) = rotIndexMinMax (fromEnum 'a' :: Int) (fromEnum 'z' :: Int) (fromEnum c :: Int)
+               | (isAsciiUpper c) = rotIndexMinMax (fromEnum 'A' :: Int) (fromEnum 'Z' :: Int) (fromEnum c :: Int)
+               | otherwise = fromEnum c :: Int
+
+rotIndexMinMax :: Int -> Int -> Int -> Int
+rotIndexMinMax i j n | (13 + n) > j = i-1 + ((13 + n) - j) -- i-1 to restore the index otherwise it skips 'a'
+                     | otherwise = 13 + n
+
+
+rot13TestLetters = quickCheckResult (\cs -> (cs /= "" && lettersOnly cs) --> (cs  == rot13 (rot13 cs)) && ((rot13 cs) /= cs))
+rot13TestNumbers = quickCheckResult (\cs -> (cs /= "" && numbersOnly cs) --> cs  == (rot13 cs))
+
+
+lettersOnly :: [Char] -> Bool
+lettersOnly [] = True
+lettersOnly (c:cs) = c `elem` ['a'..'z']++['A'..'Z'] && lettersOnly cs
+
+numbersOnly :: [Char] -> Bool
+numbersOnly [] = True
+numbersOnly (c:cs) = c `elem` ['0'..'9'] && numbersOnly cs
+
