dfb39a23ee23840e34c7ac63000cf1946ff52b1e
NL\SangaGupta <sangagupta@deloitte.nl>
2017-09-19 13:02:18 +0200

    Added simple test

--- sangam/lab3/Exc4.hs
+++ sangam/lab3/Exc4.hs
@@ -17,30 +17,72 @@ import Lecture3
     Deliverables: generator for formulas, sequence of test properties,
     test report, indication of time spent.
 
-    Time spent: 
+    Time spent:
 --------------------------------------------------------------------------------------------------------------------------------------}
 
 getRandomInt :: IO Int
 getRandomInt = getStdRandom (randomR (0,4))
 
+randomForm :: Int -> IO Form
 randomForm 0 = do Prop <$> getRandomInt
 randomForm d = do q <- getRandomInt
+                  k <- randomForm (d-1)
+                  m <- randomForm (d-1)
                   case q of
                     0 -> do
-                        Neg <$> randomForm (d-1)
+                        return $ Neg k
                     1 -> do
-                        k <- randomForm (d-1)
-                        m <- randomForm (d-1)
                         return $ Equiv (k) (m)
                     2 -> do
-                        k <- randomForm (d-1)
-                        m <- randomForm (d-1)
                         return $ Impl (k) (m)
                     3 -> do
-                        k <- randomForm (d-1)
-                        m <- randomForm (d-1)
-                        return $ Cnj[(k), (m)] -- Limited to two
+                        return $ Cnj [(k), (m)] -- Limited to two
                     4 -> do
-                        k <- randomForm (d-1)
-                        m <- randomForm (d-1)
-                        return $ Dsj[(k), (m)] -- Limited to two
+                        return $ Dsj [(k), (m)] -- Limited to two
+
+test = do
+          print "Generated Form: "
+          x <- randomForm 2
+          print x
+          print "convertToCNF Form:"
+          print (convertToCNF x)
+          print "Are the same?"
+          print (equiv x (convertToCNF x))
+
+tautology :: Form -> Bool
+tautology f = all (\ v -> evl v f) (allVals f)
+
+-- Logical equivalence
+equiv :: Form -> Form -> Bool
+equiv a b = tautology (Equiv a b)
+
+
+convertToCNF :: Form ->  Form
+convertToCNF = toCNF . nnf . arrowfree
+    where
+        toCNF :: Form -> Form
+        toCNF (Prop x) = Prop x
+        toCNF (Neg (Prop x)) = (Neg (Prop x))
+        toCNF (Cnj fs) = Cnj (map toCNF fs)
+        toCNF (Dsj [a]) = toCNF a
+        toCNF (Dsj (f1:f2)) = disLaw (toCNF f1) (toCNF (Dsj f2))
+
+        disLaw :: Form -> Form -> Form
+        disLaw (Cnj []) _ = Cnj []
+        disLaw (Cnj [f1]) f2 = disLaw f1 f2
+        disLaw (Cnj (f11:f12)) f2 = Cnj [(disLaw f11 f2), (disLaw (Cnj f12) f2)]
+        disLaw f1 (Cnj (f21:f22)) = Cnj [(disLaw f1 f21), (disLaw (Cnj f22) f1)]
+        disLaw f1 f2 = Dsj [f1, f2]
+
+a = Prop 1
+b = Prop 2
+c = Prop 3
+d = Prop 4
+
+-- Should be (after cnf): (A ∧ ((B ∨ C) ∧ (B ∨ D)))
+form4 = Cnj [a, (Dsj [b, (Cnj [c,d])])]
+
+-- Truth Table, origin vs converted
+ttOriginVSConverted f = zipWith (==) (map (\ v -> evl v f) (allVals f)) (map (\ v -> evl v (convertToCNF f)) (allVals f))
+
+r12 = parse "+(+(-1 2))"
