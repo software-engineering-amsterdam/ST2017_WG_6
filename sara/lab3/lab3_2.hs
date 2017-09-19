f3bc6bf1f336f58aceb3f029fdf034535bf91093
sara <sara@mollie.com>
2017-09-19 10:25:10 +0200

    Defining properties

--- sara/lab3/lab3_2.hs
+++ sara/lab3/lab3_2.hs
@@ -6,6 +6,12 @@
                         - Studying lecture 2 for test methods and better grasping of test properties
                         - Considering approaches, proposed them to Hugo & Ana
                         - Finalizing approach, structuring and documenting
+                1h defining properties, roughly designing my test by studying parser and lexer further
+                ____
+          TOTAL:
+
+    SOURCES:    - Lecture 2 for Hoare testing and random Ints
+                - Lecture 3
 
     DESCRIPTION:
     The lecture notes of this week define a function parse for parsing propositional formulas.
@@ -19,20 +25,37 @@
 
     ---- TESTING USING GENERATORS ----
     The Hoare Test with relevance is an interesting way to perform automated tests whilst making sure that the randomly
-    generated data set is actually relevant (enough) in order to be able to call the test reliable. However, for testing
+    generated data set is actually relevant* (enough) in order to be able to call the test reliable. However, for testing
     'parse' I would require a generator that generates formulas, which is a little more complex than just random int lists.
+    *(test input is called relevant when it meets the precondition of the test.)
 
     ---- REQUIREMENTS OF MY GENERATOR ----
     If I want to do Hoare testing with relevance, it would not make sense if all of my generated formulas match the
     precondition of the test, because then the relevance would always be 1.0. So if I want to test 'parse' in an automated
     way while simultaneously exploring the concept of relevance, my formula generator must return imperfect formulas as
-    well as proper formulas.
+    well as proper formulas. This means I will let my generator throw in some characters that are not recognized as tokens
+    by the lexer. After drawing conclusions, I will remove the invalid characters from the domain of the generator to
+    verify 100% relevance.
 
-{----------------------------------------------------------------------------------------------------------------------}
+    ---- TEST PROPERTIES ----
+    - 'parse' only accepts Strings and throws an error when unknown tokens are passed. So;
+    PRECONDITION: Any String consisting only of tokens recognized by the lexer: ['(', ')', '*', '+', '-', "==>" "<=>"]
+
+    - Leftover tokens are not shown in the result of the function 'parse'.
+    - If input is unparsable, output must be empty.
+      - Input is unparsable when order of tokens conflicts with grammar rules of lexer (to conclude from its implementation).
+      - Input is unparsable when number of opening parenthesis does not equal the number of closing parenthesis.
+
+    POSTCONDITION: If parsable, the String representation of the result must be equal to the input (ignoring spaces).
+                   Otherwise, the result must be the empty list.
+
+-----------------------------------------------------------------------------------------------------------------------}
 
 
-module Lab3_2 where
- import Lecture3
+module Lab3_2
+where
+import Lecture2
+import Lecture3
 
 
 
