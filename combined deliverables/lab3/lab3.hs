f5e59f540a4724afad467f490907d7eec4422c01
Tim Nederveen <tim.nederveen@hotmail.com>
2017-09-24 16:39:22 +0200

    Adding extra tests for ex 1

--- combined deliverables/lab3/lab3.hs
+++ combined deliverables/lab3/lab3.hs
@@ -3,6 +3,7 @@ module Lab3 where
 import Data.List
 import Test.QuickCheck
 import qualified Exc1
+import qualified Exc1ExtraTests
 import qualified Exc2
 import qualified Exc3
 import qualified Exc4
@@ -10,13 +11,16 @@ import qualified Exc5
 
 main = do
     putStrLn "\n*** Lab3 output for 1. Propositional logic notions ***"
+    putStrLn "\n* Run manual tests *"
     Exc1.main
+    putStrLn "\n* Run automated test *"
+    Exc1ExtraTests.main
 
     putStrLn "\n*** Lab3 output for 2. Testing parse function ***"
     Exc2.main
 
     putStrLn "\n*** Lab3 output for 3. CNF converter ***"
-    putStrLn "*** Skipping, as converter is tested in exercise 4 ***"
+    putStrLn "* Skipping, as converter is tested in exercise 4 *"
 
     putStrLn "\n*** Lab3 output for 4. Formula generator ***"
     Exc4.main
