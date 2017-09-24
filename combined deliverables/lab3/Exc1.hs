51f78f60c64d384806dc4a7db139862677c6a1b2
Meess <abcmaster8@hotmail.com>
2017-09-24 14:08:21 +0200

    Added types, and but grouped types of same type functions Exercise 1, added main

--- combined deliverables/lab3/Exc1.hs
+++ combined deliverables/lab3/Exc1.hs
@@ -1,12 +1,19 @@
 {-
-Assignment:		Lab 3: Assignment 1
+Assignment:		  Lab 3: Assignment 1
 Name:           Tim Nederveen
-Time spent:     TODO
+Time spent:     1 hour
+------
+Assignment:
+The lecture notes of this week discuss the notions of satisfiability, tautology, 
+contradiction, logical entailment and logical equivalence for formulas of propositional 
+logic. The lecture notes give a definition of satisfiable, for objects of type Form.
 
-Remarks:        - 
-
-Sources:        - 
----------------}
+Your task is to give definitions of:
+  contradiction
+  tautology
+  entails
+  equiv
+----------------------------------------------------------------------------------------}
 
 module Exc1 where
 
@@ -14,43 +21,25 @@ import Data.List
 import System.Random
 import Lecture3
 
-{-------------------------------------------------------------------------------------------------------------------------------------
-Exercise Implementing and testing IBAN validation
-
-a)  The lecture notes of this week discuss the notions of satisfiability, tautology, contradiction, logical entailment and logical equivalence for formulas of propositional logic.
-
-The lecture notes give a definition of satisfiable, for objects of type Form.
-
-Your task is to give definitions of:
- contradiction
- tautology
- entails
- equiv
---------------------------------------------------------------------------------------------------------------------------------------}
-
-
 {--
-    We test the implemented specifications by using propositions p q and r, and form1, form2, form3 as specified in Lecture3.hs. 
-    For each function, input for at least one valid and one non-valid result is tested, 
-    using expressions of which the expected outcome is known.
+We test the implemented specifications by using propositions p q and r, and form1, form2, 
+form3 as specified in Lecture3.hs. For each function, input for at least one valid and 
+one non-valid result is tested, using expressions of which the expected outcome is known.
 --}
 
-contradiction :: Form -> Bool
-contradiction f = not (satisfiable f) 
-
-tautology :: Form -> Bool
+contradiction, tautology :: Form -> Bool
+contradiction f = not (satisfiable f)
 tautology f = all (`evl` f) (allVals f)
 
-
--- | logical entailment: B logically entails A is true if and only if it is necessary that if all of the elements of B are true, then A is true.
-entails :: Form -> Form -> Bool
-entails f g = tautology (Impl f g)
-
+-- | logical entailment: B logically entails A is true if and only if it is necessary 
+-- that if all of the elements of B are true, then A is true.
 -- | logical equivalence: Equivalence if A and B are always equivalent
-equiv :: Form -> Form -> Bool
+entails, equiv :: Form -> Form -> Bool
+entails f g = tautology (Impl f g)
 equiv f g = tautology (Equiv f g)
 
 
+testCont, testTaut, testEntail, testEquiv :: Bool
 testCont   = and [contradiction (Neg form1),
                   not (contradiction (form2)),
                   contradiction (Neg form3)]
@@ -65,6 +54,7 @@ testEntail = and [not (entails form1 form2),
 testEquiv  = and [equiv (Impl (Neg q) (Neg p)) (Impl p q),
                   not (equiv (Impl q p) (Impl q r))]
 
+manualTests :: IO ()
 manualTests
     | not testCont = print "Contradiction tests failed"
     | not testTaut = print "Tautology tests failed"
@@ -72,3 +62,4 @@ manualTests
     | not testEquiv = print "Equivalence tests failed"
     | otherwise = print "++ All manual set test cases are valid"
 
+main = manualTests
