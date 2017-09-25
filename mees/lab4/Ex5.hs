a8ea003e30aa7653ba4e7d775e445b273563b363
Meess <abcmaster8@hotmail.com>
2017-09-25 16:34:08 +0200

    Cleanup redundant code and comments

--- mees/lab4/Ex5.hs
+++ mees/lab4/Ex5.hs
@@ -2,29 +2,13 @@ module Ex5 where
 
 import Data.List
 import Data.Tuple
+import Lab4
 import System.Random
 import Test.QuickCheck
 import SetOrd
-
-type Rel a = [(a,a)]
-
--- aas :: Rel a
--- aas = [(1,2)]
 
 inverseSet :: Rel a -> Rel a
 inverseSet = map swap
 
 symClos :: Ord a => Rel a -> Rel a
 symClos x = nub (union x (inverseSet x))
-
--- (list2set x)
--- (list2set (inverseSet [(1,2),(3,4)]))
-
-
-
--- [(1,2),(3,4)]
--- unionSet (list2set x) (list2set (inverseSet x))
-
-
--- list2set (inverseSet [(1,2),(3,4)])
--- [(1,2),(3,4)]
