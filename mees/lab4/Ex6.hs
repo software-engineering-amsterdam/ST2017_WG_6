58c7e8d17deb3877b71e58acfcc2c1801ca9fe6a
Meess <abcmaster8@hotmail.com>
2017-09-25 16:40:48 +0200

    substituted union for ++

--- mees/lab4/Ex6.hs
+++ mees/lab4/Ex6.hs
@@ -10,10 +10,14 @@ trClos :: Ord a => Rel a -> Rel a
 trClos x = sort(helper x x)
     where
         helper x [] = []
-        helper x y = union y (helper x (x @@ y))
+        helper x y = y ++ (helper x (x @@ y))
 
 -- Second (also valid) solution with use of fix
 trClos' :: Ord a => Rel a -> Rel a
 trClos' x = sort ((x,x) $$ fix (\ f (x,y) -> if y == [] 
                                              then [] 
-                                             else union y (f (x,(x @@ y)))))
+                                             else y ++ (f (x,(x @@ y)))))
+
+-- trClos'' :: Ord a => Rel a -> Rel a
+-- trClos'' x = until (y == []) (y ++ (x @@ y))
+
