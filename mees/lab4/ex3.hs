aabaefc81e44a6259e5cd8faf5b212e368b25b48
Meess <abcmaster8@hotmail.com>
2017-09-26 00:50:23 +0200

    Code exc3, TODO quickcheck

---
+++ mees/lab4/ex3.hs
@@ -0,0 +1,12 @@
+module Ex3 where
+
+import Data.List
+import System.Random
+import Test.QuickCheck 
+import SetOrd
+import Lab4
+
+unionSSet, intersectSet, diffSet :: (Ord a) => Set a -> Set a -> Set a
+unionSSet    (Set x) (Set y) = Set $ x ++ y
+diffSet      (Set x) (Set y) = Set $ x \\ y
+intersectSet (Set x) (Set y) = Set $ x \\ (x \\ y)
