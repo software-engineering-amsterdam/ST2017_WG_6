d4a93f75edb23c335a14dcdfeeb8821f30f8b085
Meess <abcmaster8@hotmail.com>
2017-09-25 22:59:29 +0200

    Used fp for fixed point

--- mees/lab4/Ex6.hs
+++ mees/lab4/Ex6.hs
@@ -1,22 +1,9 @@
 module Ex6 where
 
 import Data.List
-import Lab4
-import Data.Tuple
 import Lecture4
-import SetOrd
+import Lab4
 
--- TODO 
--- CYCLIC relation as [(1,2),(2,1),(2,2)]  crashes!
 trClos :: Ord a => Rel a -> Rel a
-trClos x = sort(helper x x)
-    where
-        helper x [] = []
-        helper x y = y ++ (helper x (x @@ y))
-
--- Second (also valid) solution with use of fix
-trClos' :: Ord a => Rel a -> Rel a
-trClos' x = sort ((x,x) $$ fix (\ f (x,y) -> if y == [] 
-                                             then [] 
-                                             else y ++ (f (x,(x @@ y)))))
-
+trClos x = fp helper x
+           where helper x = sort(nub(x ++ (x @@ x)))
