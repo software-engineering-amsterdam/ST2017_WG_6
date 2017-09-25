5c05373c90097cf53ad923a31a88141bccf72070
Meess <abcmaster8@hotmail.com>
2017-09-26 00:51:50 +0200

    Added genIntList from Lecture2.hs

--- mees/lab4/Lab4.hs
+++ mees/lab4/Lab4.hs
@@ -1,6 +1,7 @@
 module Lab4
 where 
 import Data.List
+import System.Random
 
 type Rel a = [(a,a)]
 
@@ -13,3 +14,26 @@ r @@ s =
 -- Source: https://hackage.haskell.org/package/Unique-0.4.7.1/docs/src/Data-List-Unique.html#sortUniq
 sortUniq :: Ord a => [a] -> [a]
 sortUniq = nub . sort
+
+-- Source:  Lecture2.hs
+getRandomInt :: Int -> IO Int
+getRandomInt n = getStdRandom (randomR (0,n))
+
+randomFlip :: Int -> IO Int
+randomFlip x = do
+   b <- getRandomInt 1
+   if b==0 then return x else return (-x)
+
+genIntList :: IO [Int]
+genIntList = do
+  k <- getRandomInt 20
+  n <- getRandomInt 10
+  getIntL k n
+
+getIntL :: Int -> Int -> IO [Int]
+getIntL _ 0 = return []
+getIntL k n = do
+   x <-  getRandomInt k
+   y <- randomFlip x
+   xs <- getIntL k (n-1)
+   return (y:xs)
