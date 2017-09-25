8e671e209475e7fc527d51741aa40647fbf33957
Meess <abcmaster8@hotmail.com>
2017-09-25 23:21:58 +0200

    Removed redundant libs, right nameing, changed look of code, shit works now

--- mees/lab4/Ex6.hs
+++
@@ -1,9 +0,0 @@
-module Ex6 where
-
-import Data.List
-import Lecture4
-import Lab4
-
-trClos :: Ord a => Rel a -> Rel a
-trClos x = fp helper x
-           where helper x = sort(nub(x ++ (x @@ x)))
--- mees/lab4/Lab4.hs
+++ mees/lab4/Lab4.hs
@@ -9,3 +9,7 @@ infixr 5 @@
 (@@) :: Eq a => Rel a -> Rel a -> Rel a
 r @@ s = 
   nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
+-- Source: https://hackage.haskell.org/package/Unique-0.4.7.1/docs/src/Data-List-Unique.html#sortUniq
+sortUniq :: Ord a => [a] -> [a]
+sortUniq = nub . sort
