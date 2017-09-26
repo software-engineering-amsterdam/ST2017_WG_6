4122ff909944ab98f04bf3f3d4cc4292d9ba8542
sara <sara@mollie.com>
2017-09-26 14:50:01 +0200

    Lab 4 ex 3 expanded with properties

--- sara/lab4/lab4_3.hs
+++ sara/lab4/lab4_3.hs
@@ -2,7 +2,13 @@
     Assignment:		Lab 4: Exercise 3 - Implementing and testing set operations
     Name:           Sara Oonk
     Time spent:     2h 30m
-    Sources:        SetOrd.hs, Lecture2
+    Sources:        SetOrd.hs
+                    Lecture2
+                    http://web.mnstate.edu/peil/MDEV102/U1/S3/Property6.htm
+                    http://www.mathcaptain.com/algebra/set-difference.html
+                    http://planetmath.org/setdifference
+                    http://www.math-only-math.com/intersection-of-sets.html
+                    http://www.webovations.com/education/mathbook/book/sets7.htm
 -}
 module Exc3 where
 import SetOrd
@@ -42,9 +48,17 @@ main = do
  automatedTests
  putStrLn "\n"
  quickChecks
+ putStrLn "\n"
+ unionTests
+ putStrLn "\n"
+ intersectionTests
+ putStrLn "\n"
+ differenceTests
+
+
 
 automatedTests = do
- putStrLn "\n**** Automated tests ****"
+ putStrLn "\n**** Automated tests for implementation ****"
  putStrLn "\n*** Intersection: ***"
  testSet testIntersection
  putStrLn "\n*** Difference: ***"
@@ -53,13 +67,40 @@ automatedTests = do
  testSet testUnion
 
 quickChecks = do
- putStrLn "\n**** QuickCheck tests ****"
+ putStrLn "\n**** QuickCheck tests for implementation ****"
  putStrLn "\n*** Intersection: ***"
  quickCheckSet testIntersection
  putStrLn "\n*** Difference: ***"
  quickCheckSet testDifference
  putStrLn "\n*** Union: ***"
  quickCheckSet testUnion
+
+unionTests = do
+ putStrLn "\n**** Automated test for union properties ****"
+ putStrLn "\n*** Commutative law: ***"
+ testSet commutativeUnion
+ putStrLn "\n*** Law of identity element: ***"
+ testSet identityUnion
+ putStrLn "\n*** Idempotent law: ***"
+ testSet idempotentUnion
+ 
+intersectionTests = do
+ putStrLn "\n**** Automated test for intersection properties ****"
+ putStrLn "\n*** Commutative law: ***"
+ testSet commutativeIntersection
+ putStrLn "\n*** Law of identity element: ***"
+ testSet identityIntersection
+ putStrLn "\n*** Idempotent law: ***"
+ testSet idempotentIntersection
+ 
+differenceTests = do
+ putStrLn "\n**** Automated test for difference properties ****"
+ putStrLn "\n*** Commutative law: ***"
+ testSet commutativeDifference
+ putStrLn "\n*** Law of identity element: ***"
+ testSet identityDifference
+ putStrLn "\n*** Idempotent law: ***"
+ testSet idempotentDifference
 
 
 -- Automated tests
@@ -71,12 +112,12 @@ testS k n c = if k == n then print (show n ++ " tests passed")
                 else do
                   xs <- genIntList
                   ys <- genIntList
-                  if (c (nub(sort xs)) (nub(sort ys))) then
+                  if (c (nub $ sort xs) (nub $ sort ys)) then
                        testS (k+1) n c
-                  else error ("failed test on: " ++ show (nub(sort xs)) ++ " with " ++ show (nub(sort ys)))
+                  else error ("failed test on: " ++ show (nub $ sort xs) ++ " with " ++ show (nub $ sort ys))
 
 
--- Test properties using Haskell's Data.List operations
+-- Test implementations using Haskell's Data.List operations
 testIntersection, testDifference, testUnion :: [Int] -> [Int] -> Bool
 testIntersection xs ys = (intersectionSet (Set xs) (Set ys)) == list2set (intersect xs ys)
 testDifference   xs ys = (differenceSet (Set xs) (Set ys))   == list2set (xs \\ ys)
@@ -84,4 +125,24 @@ testUnion        xs ys = (unionSet (Set xs) (Set ys))        == list2set (xs `un
 
 -- QuickCheck using above properties for 'setOp'
 quickCheckSet :: ([Int] -> [Int] -> Bool) -> IO Result
-quickCheckSet setOp = quickCheckResult (\xs -> \ys -> setOp (nub(sort xs)) (nub(sort ys)))
+quickCheckSet setOp = quickCheckResult (\xs -> \ys -> setOp (nub $ sort xs) (nub $ sort ys))
+
+-- Properties for union
+commutativeUnion, identityUnion, idempotentUnion :: [Int] -> [Int] -> Bool
+commutativeUnion xs ys = (unionSet (Set xs) (Set ys)) == (unionSet (Set ys) (Set xs))
+identityUnion    xs ys = (unionSet (Set xs) (Set [])) == (Set xs)
+idempotentUnion  xs ys = (unionSet (Set xs) (Set xs)) == (Set xs)
+
+-- Properties for intersection
+commutativeIntersection, identityIntersection, idempotentIntersection :: [Int] -> [Int] -> Bool
+commutativeIntersection xs ys = (intersectionSet (Set xs) (Set ys)) == (intersectionSet (Set ys) (Set xs))
+identityIntersection    xs ys = (intersectionSet (Set xs) (Set [])) == (Set [])
+idempotentIntersection  xs ys = (intersectionSet (Set xs) (Set xs)) == (Set xs)
+
+-- Properties for difference
+commutativeDifference, identityDifference, idempotentDifference :: [Int] -> [Int] -> Bool
+commutativeDifference xs ys = xs /= ys --> (differenceSet (Set xs) (Set ys)) /= (differenceSet (Set ys) (Set xs))
+identityDifference    xs ys = (differenceSet (Set xs) (Set [])) == (Set xs)
+idempotentDifference  xs ys = (differenceSet (Set xs) (Set xs)) == (Set [])
+
+
