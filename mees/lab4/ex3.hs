8ec0bb7ee1eecc36447ebf3a32893d79e2598168
Meess <abcmaster8@hotmail.com>
2017-09-26 09:40:03 +0200

    Set -> list2set to prevent doubles

--- mees/lab4/ex3.hs
+++ mees/lab4/ex3.hs
@@ -7,6 +7,6 @@ import SetOrd
 import Lab4
 
 unionSSet, intersectSet, diffSet :: (Ord a) => Set a -> Set a -> Set a
-unionSSet    (Set x) (Set y) = Set $ x ++ y
-diffSet      (Set x) (Set y) = Set $ x \\ y
-intersectSet (Set x) (Set y) = Set $ x \\ (x \\ y)
+unionSSet    (Set x) (Set y) = list2set $ x ++ y
+diffSet      (Set x) (Set y) = list2set $ x \\ y
+intersectSet (Set x) (Set y) = list2set $ x \\ (x \\ y)
