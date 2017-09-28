c2512609413e0983810753053cccad573a115eb9
sara <sara@mollie.com>
2017-09-28 08:26:18 +0200

    4_2

--- sara/lab4/lab4_2.hs
+++ sara/lab4/lab4_2.hs
@@ -4,7 +4,6 @@
     Time spent:     15m
     Sources:
 
-
     Implement a random data generator for the datatype Set Int, where Set is as defined in SetOrd.hs.
     First do this from scratch, next give a version that uses QuickCheck to random test this datatype.
     (Deliverables: two random test generators, indication of time spent.)
@@ -20,8 +19,8 @@ genRandomSet = do
  xs <- genIntList
  return (list2set xs)
 
+quickCheckRandomSet :: IO ()
+quickCheckRandomSet = quickCheck (\xs-> (testSet xs))
 
-quickCheckRandomSet = quickCheck (\xs -> (testSet xs) == (list2set xs))
-
-testSet :: [Int] -> Set Int
-testSet xs = (list2set xs)
+testSet :: [Int]-> Bool
+testSet xs = (list2set xs) == Set (nub $ sort xs)
