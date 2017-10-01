7ba87bf903857f9d7dd1b02aaa7e3346b431b4f6
Meess <abcmaster8@hotmail.com>
2017-10-01 10:11:15 +0200

    Ex8 comments

--- mees/lab4/Ex8.hs
+++ mees/lab4/Ex8.hs
@@ -1,3 +1,8 @@
+-- ################################################
+-- # Mees Kalf
+-- # Excersize 8
+-- # 2 hour(s)
+-- ################################################
 module Ex8 where
 
 import Data.List
@@ -8,17 +13,25 @@ import SetOrd
 import Ex5
 import Ex6
 
+-- Create the transitive closure of the symmetric closure and the symmetric
+-- closure of the transitive closure
 trClosSymClos :: Ord a => Rel a -> Rel a
 trClosSymClos x = trClos(symClos(x))
 
 symClosTrClos :: Ord a => Rel a -> Rel a
 symClosTrClos x = symClos(trClos(x))
 
+-- Check if the trClosSymClos and symClosTrClos are commutative, this test
+-- can be executed with quickCheck, we see it fails: they are not commutative.
+-- Using verboseCheck gives us the imput on which it crashes.
 test8 :: Integral a => Rel a -> Bool
 test8 x = trClosSymClos x == symClosTrClos x
 
-{-------------------------------------------------------------------------------------
+{------------------------------------------------------------------------------
     
+Assignment 8
+Example that illustrates the difference:
+
 Take a simple relation: [(0,1)]
 
 If we first take the transative closure we get:
@@ -27,7 +40,8 @@ If we first take the transative closure we get:
 Then calculating the symmetric closure gives:
 [(0,1),(1,0)]
 
-If we calculate then the other way around and first apply the symmetric closure:
+If we calculate them the other way around and first apply the symmetric closure
+on [(0,1)] we get:
 [(0,1),(1,0)]
 
 And then calulate the transitive closure of [(0,1),(1,0)], we get:
@@ -35,4 +49,4 @@ And then calulate the transitive closure of [(0,1),(1,0)], we get:
 
 Since [(0,1),(1,0)] is NOT [(0,0),(0,1),(1,0),(1,1)] therefore they are not
 commutative.
---------------------------------------------------------------------------------------}
+------------------------------------------------------------------------------}
