c91e2fdc139a99ab258ffc0bab80b64171c94554
Meess <abcmaster8@hotmail.com>
2017-10-01 09:55:45 +0200

    commented Ex5 and replaced ++ with union

--- mees/lab4/Ex5.hs
+++ mees/lab4/Ex5.hs
@@ -1,3 +1,8 @@
+-- ################################################
+-- # Mees Kalf
+-- # Excersize 5
+-- # 1,5 hour(s)
+-- ################################################
 module Ex5 where
 
 import Data.List
@@ -5,5 +10,7 @@ import Data.Tuple
 import Lab4
 import Lecture4
 
+-- As denoted on the wikipedia page : https://en.wikipedia.org/wiki/Symmetric_closure
+-- the symetric closure of relation R is the union of R with its inverse relation R-
 symClos :: Ord a => Rel a -> Rel a
-symClos x = sortUniq $ x ++ (map swap x)
+symClos x = sortUniq $  union x (map swap x)
