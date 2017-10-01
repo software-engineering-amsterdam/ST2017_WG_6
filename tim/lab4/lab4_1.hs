14ea1fdbe7ca733eb797b7a9ec4c12c217f1463c
Tim Nederveen <tim.nederveen@hotmail.com>
2017-10-01 16:37:58 +0200

    exc 10 finished

---
+++ tim/lab4/lab4_1.hs
@@ -0,0 +1,5 @@
+module Exc1 where
+
+import Data.List
+import System.Random
+import Test.QuickCheck   
---
+++ tim/lab4/lab4_2.hs
@@ -0,0 +1,11 @@
+module Exc2 where
+
+import Data.List
+import System.Random
+import Test.QuickCheck   
+
+-- use listToSet method, and a list of random integers
+
+-- Is there a difference between the symmetric closure of the transitive closure of a relation RR and the transitive closure of the symmetric closure of RR?
+-- Deliverable: If your answer is that these are the same, you should give an argument, if you think these are different you should give an example that illustrates the difference.
+
