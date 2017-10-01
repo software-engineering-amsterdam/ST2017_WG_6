393496305421bb429b92421f677dd7ab30abef32
Meess <abcmaster8@hotmail.com>
2017-10-01 10:14:36 +0200

    ex3 comments

--- mees/lab4/ex3.hs
+++ mees/lab4/ex3.hs
@@ -1,3 +1,8 @@
+-- ################################################
+-- # Mees Kalf
+-- # Excersize 3
+-- # 0.5 hour(s)
+-- ################################################
 module Ex3 where
 
 import Data.List
@@ -6,6 +11,10 @@ import Test.QuickCheck
 import SetOrd
 import Lab4
 
+-- We read the set input as a list, then compute the union, difference
+-- or intersect of these lists and convert them back to a set. Although
+-- it is a kind of a dirty implentation it certainly works and is a short
+-- and clean solution.
 unionSSet, intersectSet, diffSet :: (Ord a) => Set a -> Set a -> Set a
 unionSSet    (Set x) (Set y) = list2set $ union x y
 diffSet      (Set x) (Set y) = list2set $ x \\ y
