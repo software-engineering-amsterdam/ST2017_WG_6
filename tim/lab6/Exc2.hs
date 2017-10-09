28a2669a69009e56fdabed1e354c89b2155790aa
Tim Nederveen <tim.nederveen@hotmail.com>
2017-10-09 15:32:45 +0200

    Finished Exc1

---
+++ tim/lab6/Exc2.hs
@@ -0,0 +1,23 @@
+{-
+Assignment:		Lab 6: Exercise 2
+Name:           Tim Nederveen
+Time spent:
+
+Remarks:        
+Sources:        
+---------------}
+
+module Exc2 where
+
+import Data.List
+import Lecture6
+import Exc1
+
+{-
+
+Exercise 2
+
+Check that your implementation is more efficient than expM by running a number of relevant tests and documenting the results.
+
+-}
+
