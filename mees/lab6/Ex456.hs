3d8e1188385ad0d1603bab1538b371126e94dad6
Meess <abcmaster8@hotmail.com>
2017-10-09 19:07:23 +0200

    Whoehoee shit works, shit is slow

--- mees/lab6/Ex456.hs
+++ mees/lab6/Ex456.hs
@@ -11,22 +11,6 @@ carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) |
       prime (12*k+1),
       prime (18*k+1) ]
 
-testPtf nt x y = do
-    semiPrime <- primeTestsF nt x
-    when (not(semiPrime == prime x)) (print ("Failed " ++ show x))
-    when (x /= y) (testPtf nt (x+1) y)
-
-testPtf nt x y = do
-    let car = carmichael !! (fromIntegral y)
-    semiPrime <- primeTestsF nt car
-    when (not(semiPrime == prime car)) (print ("Failed " ++ show x))
-    when (x /= y) (testPtf nt (x+1) y)
-
-testPtf nt x y = do
-    let car = carmichael !! (fromIntegral y)
-    semiPrime <- primeMR nt car
-    when (not(semiPrime == prime car)) (print ("Failed " ++ show x))
-    when (x /= y) (testPtf nt (x+1) y)
 
 testPrime _ [] _= print "Finished"
 testPrime nt (x:xs) f = do
@@ -34,4 +18,8 @@ testPrime nt (x:xs) f = do
                         when (not(semiPrime == prime x)) (print ("Failed " ++ show x))
                         testPrime nt xs f
 
-test = testPrime 1 (take 100 composites) primeTestsF
+testEx4 = testPrime 1 (take 100 composites) primeTestsF
+
+testEx5 = testPrime 1 (take 2 carmichael) primeTestsF
+
+testEx6 = testPrime 1 (take 2 carmichael) primeMR
