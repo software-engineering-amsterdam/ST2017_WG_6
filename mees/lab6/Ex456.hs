6eb1e9ffb909584773a2b3740815a25b49e7d661
Meess <abcmaster8@hotmail.com>
2017-10-09 19:14:51 +0200

    Made functions dynamic

--- mees/lab6/Ex456.hs
+++ mees/lab6/Ex456.hs
@@ -11,15 +11,17 @@ carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) |
       prime (12*k+1),
       prime (18*k+1) ]
 
-
+testPrime :: t -> [Integer] -> (t -> Integer -> IO Bool) -> IO ()
 testPrime _ [] _= print "Finished"
 testPrime nt (x:xs) f = do
                         semiPrime <- f nt x
                         when (not(semiPrime == prime x)) (print ("Failed " ++ show x))
                         testPrime nt xs f
 
-testEx4 = testPrime 1 (take 100 composites) primeTestsF
-
-testEx5 = testPrime 1 (take 2 carmichael) primeTestsF
-
-testEx6 = testPrime 1 (take 2 carmichael) primeMR
+--      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- BE AWARE testEx5 and testEx6 EXTREMELY SLOW start with nt = 1 n = 1
+--      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+testEx4, testEx5, testEx6 :: Int -> Int -> IO ()
+testEx4 nt n = testPrime nt (take n composites) primeTestsF
+testEx5 nt n = testPrime nt (take n carmichael) primeTestsF
+testEx6 nt n = testPrime nt (take n carmichael) primeMR
